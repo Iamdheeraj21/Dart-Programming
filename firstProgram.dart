b61e0b296502a0f757217032ef391f6e0a0435d7
Iamdheeraj21 <dheerajprajapati334@gmail.com>
2021-07-04 11:46:37 +0530

    Cahnged

--- firstProgram.dart
+++ firstProgram.dart
@@ -1,5 +1,15 @@
 void main() {
-  for (int i = 0; i <= 4; i + 2) {
+  for (int i = 0; i <= 4; i++) {
     print('hello ${i + 2}');
   }
+
+  var heart_symbol = '\u2665';
+  var laugh_symbol = '\u{1f6}';
+  int var1 = 1000;
+
+  print(heart_symbol);
+  print(laugh_symbol);
+  print(var1);
+
+  
 }
